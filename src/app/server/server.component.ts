dad6c7386956fe341cf9c2e079d4de8e260212e8
Jay Kavathiya <jkavathiya18@gmail.com>
2017-06-29 12:44:23 -0400

    example component added

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by Jay on 6/28/2017.
+ */
+import { Component } from '@angular/core';
+
+@Component({
+  selector:'app-server',
+  templateUrl:'./server.component.html'
+})
+
+export class ServerComponent{
+
+}
