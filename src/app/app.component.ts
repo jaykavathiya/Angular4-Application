2570ccab4a0283df22c4ec4b67657a6f0e4ae7cb
Jay Kavathiya <jkavathiya18@gmail.com>
2017-07-13 15:21:42 -0400

    custom event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   serverElements = [{type:'server',name: 'Test Server',content: 'This is just a simple test'}];
+
+  onServerAdded(serverData:{serverName: string, serverContent: string}){
+    this.serverElements.push({
+      type:'server',
+      name: serverData.serverName,
+      content:serverData.serverContent
+    });
+  }
+
+  onBlueprintAdded(BlueprintData:{serverName: string, serverContent: string}){
+    this.serverElements.push({
+      type:'blueprint',
+      name: BlueprintData.serverName,
+      content:BlueprintData.serverContent
+    });
+  }
 }
